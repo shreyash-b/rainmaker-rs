827c9b06b3f4f88cd19f419d51c039a4016a4911
Shreyash Bubane <52196231+shreyash-b@users.noreply.github.com>
2024-01-04 21:52:07 +0530

    no longer need to change build.rs while compiling for linux(yay)

--- led_switch/build.rs
+++ led_switch/build.rs
@@ -1,3 +1,6 @@
-fn main(){
-    embuild::espidf::sysenv::output();
+fn main() {
+    let target_os = std::env::var_os("CARGO_CFG_TARGET_OS").expect("");
+    if target_os == "espidf" {
+        embuild::espidf::sysenv::output();
+    }
 }
