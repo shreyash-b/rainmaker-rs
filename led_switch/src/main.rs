9897d8503175eb7f5acfde46bc70bc55b8dfe131
compactdisc23 <chinmaydixit2303@gmail.com>
2024-01-23 09:48:22 +0530

    code cleaning

--- led_switch/src/main.rs
+++ led_switch/src/main.rs
@@ -11,27 +11,27 @@ fn main() {
 
     rainmaker::rainmaker_init();
     rainmaker::rainmaker_say_hello();
+    
+    /* ESP specific code : WiFi library integration to be done... */
+    // let peripherals = esp_idf_svc::hal::peripherals::Peripherals::take().unwrap();
+    // let sysloop = esp_idf_svc::eventloop::EspSystemEventLoop::take().unwrap();
+    // let nvs = esp_idf_svc::nvs::EspDefaultNvsPartition::take().unwrap();
 
-    // ESP specific code : WiFi library integration to be done...
-    let peripherals = esp_idf_svc::hal::peripherals::Peripherals::take().unwrap();
-    let sysloop = esp_idf_svc::eventloop::EspSystemEventLoop::take().unwrap();
-    let nvs = esp_idf_svc::nvs::EspDefaultNvsPartition::take().unwrap();
+    // let mut wifi = esp_idf_svc::wifi::BlockingWifi::wrap(
+    //     esp_idf_svc::wifi::EspWifi::new(peripherals.modem, sysloop.clone(), Some(nvs)).unwrap(),
+    //     sysloop,
+    // ).unwrap();
 
-    let mut wifi = esp_idf_svc::wifi::BlockingWifi::wrap(
-        esp_idf_svc::wifi::EspWifi::new(peripherals.modem, sysloop.clone(), Some(nvs)).unwrap(),
-        sysloop,
-    ).unwrap();
+    // wifi.set_configuration(&esp_idf_svc::wifi::Configuration::Client(esp_idf_svc::wifi::ClientConfiguration{
+    //     ssid: "nothing phone 2".into(),
+    //     password: "LOWERCASE".into(),
+    //     ..Default::default()
+    // })).unwrap();
 
-    wifi.set_configuration(&esp_idf_svc::wifi::Configuration::Client(esp_idf_svc::wifi::ClientConfiguration{
-        ssid: "nothing phone 2".into(),
-        password: "LOWERCASE".into(),
-        ..Default::default()
-    })).unwrap();
+    // wifi.start().unwrap();
+    // wifi.connect().unwrap();
 
-    wifi.start().unwrap();
-    wifi.connect().unwrap();
-
-    esp_idf_svc::hal::delay::Delay::new_default().delay_ms(5000);
+    // esp_idf_svc::hal::delay::Delay::new_default().delay_ms(5000);
 
     /*                  MQTT Client connection , subscribe, publish                */
 
@@ -50,7 +50,7 @@ fn main() {
             MqttEvent::Disconnected => log::error!("MQTT Disconnected"),
             MqttEvent::BeforeConnect => log::warn!("MQTT Connecting"),
             MqttEvent::Received => log::info!("Message Published"),
-            _ => log::warn!("unknown event"),
+            _ => log::warn!("Unaddressed Event"),
         }),
     )
     .unwrap();
@@ -58,7 +58,7 @@ fn main() {
     client.publish(
     "node/58CF79DA4FD0/params/local",
     &mqtt::QoSLevel::AtLeastOnce,
-    "{\"Light\":{\"Name\":\"Light\",\"Power\":true,\"Brightness\":50,\"Hue\":360,\"Saturation\":100},}".into()
+    "{\"Light\":{\"Name\":\"Light\",\"Power\":true,\"Brightness\":40,\"Hue\":270,\"Saturation\":100},}".into()
     );
 
     client.subscribe(
