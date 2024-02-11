04041c25d204d80b09cb0d8e99e1d596c4f2a412
Shreyash Bubane <52196231+shreyash-b@users.noreply.github.com>
2024-02-11 22:17:03 +0530

    merged mqtt branch

---
+++ components/src/mqtt/base.rs
@@ -0,0 +1,37 @@
+pub enum QoSLevel {
+    AtMostOnce,
+    AtLeastOnce,
+    ExactlyOnce,
+}
+
+pub struct MqttClient<T> {
+    pub(crate) client: T,
+}
+
+pub struct MqttConfiguration<'a> {
+    pub host: &'a str,
+    pub clientid: &'a str,
+    pub port: u16,
+}
+
+pub struct TLSconfiguration<'a> {
+    pub client_cert: &'a Vec<u8>,
+    pub private_key:&'a Vec<u8>,
+    pub server_cert: &'a Vec<u8>
+}
+
+#[derive(Debug)]
+pub struct PublishMessage {
+    pub topic: String,
+    pub payload: Vec<u8>,
+}
+
+#[derive(Debug)]
+pub enum MqttEvent {
+    Connected,
+    Disconnected,
+    Publish(PublishMessage),
+    BeforeConnect,
+    Received,
+    Other,
+}
---
+++ components/src/mqtt/linux.rs
@@ -0,0 +1,94 @@
+use crate::mqtt::base::*;
+
+impl From<&rumqttc::QoS> for QoSLevel {
+    fn from(input: &rumqttc::QoS) -> Self {
+        match input {
+            rumqttc::QoS::AtMostOnce => QoSLevel::AtMostOnce,
+            rumqttc::QoS::AtLeastOnce => QoSLevel::AtLeastOnce,
+            rumqttc::QoS::ExactlyOnce => QoSLevel::ExactlyOnce,
+        }
+    }
+}
+
+impl From<&QoSLevel> for rumqttc::QoS {
+    fn from(value: &QoSLevel) -> Self {
+        match value {
+            QoSLevel::AtMostOnce => rumqttc::QoS::AtMostOnce,
+            QoSLevel::AtLeastOnce => rumqttc::QoS::AtLeastOnce,
+            QoSLevel::ExactlyOnce => rumqttc::QoS::ExactlyOnce,
+        }
+    }
+}
+
+impl From<&rumqttc::Publish> for PublishMessage {
+    fn from(value: &rumqttc::Publish) -> Self {
+        Self {
+            topic: value.topic.clone(),
+            payload: value.payload.to_vec(),
+        }
+    }
+}
+
+impl From<&rumqttc::Event> for MqttEvent {
+    fn from(value: &rumqttc::Event) -> Self {
+        match value {
+            rumqttc::Event::Incoming(e) => match e {
+                rumqttc::Packet::ConnAck(_) => MqttEvent::Connected,
+                rumqttc::Packet::Publish(m) => MqttEvent::Publish(m.into()),
+                rumqttc::Packet::Disconnect => MqttEvent::Disconnected,
+                rumqttc::Packet::Connect(_) => MqttEvent::BeforeConnect,
+                _ => {
+                    log::warn!("other incoming event: {:?}", e);
+                    MqttEvent::Other
+                }
+            },
+
+            rumqttc::Event::Outgoing(e) => {
+                log::info!("other outgoing event: {:?}", e);
+                MqttEvent::Other
+            }
+        }
+    }
+}
+
+impl MqttClient<rumqttc::Client> {
+    pub fn new(
+        config: &MqttConfiguration,
+        tlscerts: &'static TLSconfiguration,
+        callback: Box<dyn Fn(MqttEvent) + Send>,
+    ) -> anyhow::Result<Self> {
+        let mut option = rumqttc::MqttOptions::new(&*config.clientid, &*config.host, config.port);
+        option.transport();
+        
+        option.set_keep_alive(std::time::Duration::from_secs(60));
+
+        option.set_transport(rumqttc::Transport::tls(
+            tlscerts.server_cert.to_vec(),
+            Some((tlscerts.client_cert.to_vec(), rumqttc::Key::RSA(tlscerts.private_key.to_vec()))),
+            None,
+        ));
+        let (client, mut conn) = rumqttc::Client::new(option, 5);
+        std::thread::spawn(move || {
+            for (_i, notification) in conn.iter().enumerate() {
+                match notification {
+                    Ok(notif) => callback(MqttEvent::from(&notif)),
+                    Err(e) => panic!("error while executing callback: {:?}", e),
+                };
+            }
+        });
+
+        Ok(Self { client })
+    }
+
+    pub fn publish(&mut self, topic: &str, qos: &QoSLevel, payload: Vec<u8>) {
+        self.client
+            .publish(topic, qos.into(), false, payload)
+            .expect("unable to publish");
+    }
+
+    pub fn subscribe(&mut self, topic: &str, qos: &QoSLevel) {
+        self.client
+            .subscribe(topic, qos.into())
+            .expect("unable to subscribe");
+    }
+}
