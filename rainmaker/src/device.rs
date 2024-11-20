02c0dc3de6a55fe2e19dce4fe9b9a3f6377982e5
compactdisc23 <chinmaydixit2303@gmail.com>
2024-11-20 11:30:32 +0530

    [doc] added docs for device

--- rainmaker/src/device.rs
+++ rainmaker/src/device.rs
@@ -1,9 +1,45 @@
+//! Device module.
+//! 
+//! One or more devices can be created and registered with node using device module.
+//! 
+//! Example for creating an instance of device:
+//! ```rust
+//! let device = Device::new(name:"DeviceName", device_type:DeviceType::Switch);
+//! let power_param = Param::new_power(name:"Power", initial_value: false);
+//! device.add_param(power_param);
+//! device.set_primary_param(param_name: "Power");
+//! ```
+//! 
+//! A callback needed to be set for every device in order to report updated values of parameters.
+//! 
+//! For the reporting purpose, the function [report_params] can be used.
+//! 
+//! Example for device callback:
+//! ```rust
+//! fn device_callback(params: HashMap<String, Value>){
+//!     /* Write code for logging the received and reported values */
+//!     rainmaker::report_params(device_name: "DeviceName", params);
+//! }
+//! ```
+//! 
+//! The callback created can be associated with device using [register_callback].
+//! ```rust
+//! device.register_callback(Box::new(device_callback)); 
+//! ```
+//! 
+//! [Param]: crate::param::Param
+//! [register_callback]: crate::device::Device::register_callback
+
+
+
 use std::{collections::HashMap, fmt::Debug};
 
 use serde::Serialize;
 use serde_json::Value;
 
 use crate::param::Param;
+#[allow(unused)]
+use crate::report_params;
 
 pub type DeviceCbType = Box<dyn Fn(HashMap<String, Value>) + Send + Sync + 'static>;
 
@@ -34,6 +70,7 @@ impl Debug for Device {
 }
 
 impl Device {
+    /// This function creates an instance of device.
     pub fn new(name: &str, device_type: DeviceType) -> Self {
         Self {
             name: name.to_owned(),
@@ -45,6 +82,7 @@ impl Device {
         }
     }
 
+    /// A parameter can be set as a primary parameter.
     pub fn set_primary_param(&mut self, param_name: &str) {
         self.primary_param = Some(param_name.to_string())
     }
@@ -55,10 +93,12 @@ impl Device {
             .expect("Failed to add attribute");
     }
 
+    /// This function associates a parameter with the device.
     pub fn add_param(&mut self, param: Param) {
         self.params.push(param);
     }
 
+    /// This function associates a callback that reports updates values of parameters.
     pub fn register_callback(&mut self, cb: DeviceCbType) {
         self.callback = Some(Box::new(cb));
     }
@@ -82,6 +122,9 @@ impl Device {
     }
 }
 
+/// Standard Predefined Types
+/// 
+/// More such type of devices can be added. Refer [device list](https://rainmaker.espressif.com/docs/standard-types).
 #[derive(Debug, Serialize)]
 pub enum DeviceType {
     #[serde(rename = "esp.device.switch")]
