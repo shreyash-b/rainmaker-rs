12b7644de1ae219b6e9cc639df192d6fc627d9fd
compactdisc23 <chinmaydixit2303@gmail.com>
2024-11-20 11:24:41 +0530

    added local/params topic

--- rainmaker/src/constants.rs
+++ rainmaker/src/constants.rs
@@ -2,3 +2,4 @@ pub const USER_MAPPING_TOPIC_SUFFIX: &str = "user/mapping";
 pub const NODE_CONFIG_TOPIC_SUFFIX: &str = "config";
 pub const NODE_PARAMS_LOCAL_INIT_TOPIC_SUFFIX: &str = "params/local/init";
 pub const NODE_PARAMS_REMOTE_TOPIC_SUFFIX: &str = "params/remote";
+pub const NODE_PARAMS_LOCAL_TOPIC_SUFFIX: &str = "params/local";
