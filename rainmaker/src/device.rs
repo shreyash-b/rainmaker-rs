841dfcda870247b6a3ca57c33365f02820ea695b
compactdisc23 <chinmaydixit2303@gmail.com>
2024-11-30 16:29:46 +0530

    [doc] add docs for device

--- rainmaker/src/device.rs
+++ rainmaker/src/device.rs
@@ -1,19 +1,21 @@
-//! Device module.
-//! 
-//! One or more devices can be created and registered with node using device module.
-//! 
+//! A module for devices. Device is a physical entity which associated with a node and can be controlled from ESP-RainMaker Dashboard. One or more devices can be created and registered with node using device module.
+//!
 //! Example for creating an instance of device:
 //! ```rust
 //! let device = Device::new(name:"DeviceName", device_type:DeviceType::Switch);
+//! ```
+//!
+//! Parameter from [Param] module can be added as following:
+//! ```rust
 //! let power_param = Param::new_power(name:"Power", initial_value: false);
 //! device.add_param(power_param);
 //! device.set_primary_param(param_name: "Power");
 //! ```
-//! 
+//!
 //! A callback needed to be set for every device in order to report updated values of parameters.
-//! 
+//!
 //! For the reporting purpose, the function [report_params] can be used.
-//! 
+//!
 //! Example for device callback:
 //! ```rust
 //! fn device_callback(params: HashMap<String, Value>){
@@ -21,16 +23,14 @@
 //!     rainmaker::report_params(device_name: "DeviceName", params);
 //! }
 //! ```
-//! 
+//!
 //! The callback created can be associated with device using [register_callback].
 //! ```rust
-//! device.register_callback(Box::new(device_callback)); 
+//! device.register_callback(Box::new(device_callback));
 //! ```
-//! 
+//!
 //! [Param]: crate::param::Param
 //! [register_callback]: crate::device::Device::register_callback
-
-
 
 use std::{collections::HashMap, fmt::Debug};
 
@@ -41,7 +41,7 @@ use crate::param::Param;
 #[allow(unused)]
 use crate::report_params;
 
-pub type DeviceCbType = Box<dyn Fn(HashMap<String, Value>) + Send + Sync + 'static>;
+pub(crate) type DeviceCbType = Box<dyn Fn(HashMap<String, Value>) + Send + Sync + 'static>;
 
 #[derive(Serialize)]
 pub struct Device {
@@ -103,10 +103,12 @@ impl Device {
         self.callback = Some(Box::new(cb));
     }
 
+    /// Function for assigning a name to device.
     pub fn name(&self) -> &str {
         &self.name
     }
 
+    /// This function associates a list of parameters to the device.
     pub fn params(&self) -> &[Param] {
         &self.params
     }
@@ -122,17 +124,55 @@ impl Device {
     }
 }
 
-/// Standard Predefined Types
-/// 
-/// More such type of devices can be added. Refer [device list](https://rainmaker.espressif.com/docs/standard-types).
+/// ESP RainMaker provides a set of standard devices. These are provided with a UI and have special handling in clients like Alexa/Google Home.
+///
+/// Refer [device list](https://rainmaker.espressif.com/docs/standard-types).
 #[derive(Debug, Serialize)]
 pub enum DeviceType {
-    #[serde(rename = "esp.device.switch")]
+    #[serde(rename = "esp.Device.Switch")]
     Switch,
-    #[serde(rename = "esp.device.lightbulb")]
+    #[serde(rename = "esp.Device.Lightbulb")]
     Lightbulb,
-    #[serde(rename = "esp.device.light")]
+    #[serde(rename = "esp.Device.Light")]
     Light,
-    #[serde(rename = "esp.device.fan")]
+    #[serde(rename = "esp.Device.Fan")]
     Fan,
+    #[serde(rename = "esp.Device.TemperatureSensor")]
+    TemperatureSensor,
+    #[serde(rename = "esp.Device.Outlet")]
+    SmartPlugOutlet,
+    #[serde(rename = "esp.Device.Plug")]
+    Smartplug,
+    #[serde(rename = "esp.Device.Socket")]
+    SmartplugSocket,
+    #[serde(rename = "esp.Device.Lock")]
+    Smartlock,
+    #[serde(rename = "esp.Device.BlindsInternal")]
+    InteriorBlind,
+    #[serde(rename = "esp.Device.BlindsExternal")]
+    ExteriorBlind,
+    #[serde(rename = "esp.Device.GarageDoor")]
+    GarageDoor,
+    #[serde(rename = "esp.Device.Speaker")]
+    Speaker,
+    #[serde(rename = "esp.Device.AirConditioner")]
+    AirConditioner,
+    #[serde(rename = "esp.Device.Thermostat")]
+    Thermostat,
+    #[serde(rename = "esp.Device.Tv")]
+    TV,
+    #[serde(rename = "esp.Device.Washer")]
+    Washer,
+    #[serde(rename = "esp.Device.ContactSensor")]
+    ContactSensor,
+    #[serde(rename = "esp.Device.MotionSensor")]
+    MotionSensor,
+    #[serde(rename = "esp.Device.Doorbell")]
+    Doorbell,
+    #[serde(rename = "esp.Device.SecurityPanel")]
+    SecurityPanel,
+    #[serde(rename = "esp.Device.WaterHeater")]
+    X,
+    #[serde(rename = "esp.Device.Other")]
+    OTHER,
 }
